629fb0c9d8bb858782cf9874cb866c4e7db0a63e
Gregor Arbeithuber <gregor.arbeithuber@hotmail.com>
2017-10-18 22:48:19 +0200

    Homework

--- Maze.Robot/RobotController.cs
+++ Maze.Robot/RobotController.cs
@@ -1,4 +1,6 @@
 ﻿using Maze.Library;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Maze.Solver
 {
@@ -8,6 +10,9 @@ namespace Maze.Solver
     public class RobotController
     {
         private IRobot robot;
+        // Saves all points that have been visited
+        private List<Point> VisitedPoints = new List<Point>();
+        private bool reachedEnd = false;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RobotController"/> class
@@ -31,15 +36,64 @@ namespace Maze.Solver
         /// </remarks>
         public void MoveRobotToExit()
         {
-            // Here you have to add your code
+            // The robot starts at 0,0
+            int x = 0;
+            int y = 0;
 
-            // Trivial sample algorithm that can just move right
-            var reachedEnd = false;
             robot.ReachedExit += (_, __) => reachedEnd = true;
 
-            while (!reachedEnd)
+            SolveMaze(x, y);
+
+            // The robot couldn't find an escape
+            if (!reachedEnd)
             {
-                robot.Move(Direction.Right);
+                robot.HaltAndCatchFire();
+            }
+        }
+        
+        public void SolveMaze(int x, int y)
+        {
+            // The point that is currently being checked
+            Point currentPoint = new Point(x, y);
+            if (!VisitedPoints.Contains(currentPoint) && !reachedEnd)
+            {
+                VisitedPoints.Add(currentPoint);
+
+                if (!reachedEnd && robot.TryMove(Direction.Up))
+                {
+                    SolveMaze(x, y - 1);
+                    if (!reachedEnd)
+                    {
+                        robot.Move(Direction.Down);
+                    }
+                }
+
+                if (!reachedEnd && robot.TryMove(Direction.Down))
+                {
+                    SolveMaze(x, y + 1);
+                    if (!reachedEnd)
+                    {
+                        robot.Move(Direction.Up);
+                    }
+                }
+
+                if (!reachedEnd && robot.TryMove(Direction.Right))
+                {
+                    SolveMaze(x + 1, y);
+                    if (!reachedEnd)
+                    {
+                        robot.Move(Direction.Left);
+                    }
+                }
+
+                if (!reachedEnd && robot.TryMove(Direction.Left))
+                {
+                    SolveMaze(x - 1, y);
+                    if (!reachedEnd)
+                    {
+                        robot.Move(Direction.Right);
+                    }
+                }
             }
         }
     }
